8ee684a919aea036a89e586196e9b9b068e189ec
amsimonsen <aubrey.simonsen@gmail.com>
2025-05-26 11:25:34 -0600

    Update design.js

--- design.js
+++ design.js
@@ -5,26 +5,6 @@ if (userInput !== password) {
   document.body.innerHTML = "<h1>Access Denied</h1>";
 }
 
-// Grid creation function
-function makeGrid() {
-  var height = document.getElementById('inputHeight').value;
-  var width = document.getElementById('inputWidth').value;
-  var table = document.getElementById('pixelCanvas');
-
-  table.innerHTML = '';
-  var tbody = document.createElement('tbody');
-
-  for (var i = 0; i < height; i++) {
-    var tr = document.createElement('tr');
-    for (var j = 0; j < width; j++) {
-      var td = document.createElement('td');
-      tr.appendChild(td);
-    }
-    tbody.appendChild(tr);
-  }
-  table.appendChild(tbody);
-}
-
 // Color fill on click (vanilla JS)
 document.addEventListener('click', function(event) {
   if (event.target.tagName === 'TD') {
