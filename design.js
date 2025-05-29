02e177f6269acb38fdecd700f31bf590c4464ed9
amsimonsen <aubrey.simonsen@gmail.com>
2025-05-28 19:30:30 -0600

    Update design.js

--- design.js
+++ design.js
@@ -1,9 +1,9 @@
 // Basic password prompt (NOT secure for sensitive info)
-//const password = "foryoureyesonly";
-//const userInput = prompt("Enter the password to view this site:");
-//if (userInput !== password) {
-//  document.body.innerHTML = "<h1>Access Denied</h1>";
-//}
+const password = "foryoureyesonly";
+const userInput = prompt("Enter the password to view this site:");
+if (userInput !== password) {
+  document.body.innerHTML = "<h1>Access Denied</h1>";
+}
 /*
 document.addEventListener('click', function(event) {
   // Find the closest td element
